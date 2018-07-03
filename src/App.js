ed0b3fba06a9c5ea81296cebab9b4c8739849b1e
Dylan Pinn <dylan@dylanpinn.com>
2018-07-04 00:51:27 +1000

    AddTodo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import AddTodo from './components/addTodo';
 
 const App = () => (
   <div>
-    My App
+    <h1>
+      Todo list
+    </h1>
+    <AddTodo submitTodo={() => {}} />
   </div>
 );
 
